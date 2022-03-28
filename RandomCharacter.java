d771f7841f6fd3e4d15cf4f1f06f3b050bb4a136
Jourdan <47164619+Jerdannn@users.noreply.github.com>
2022-03-28 22:50:18 +0800

    Add files via upload

---
+++ RandomCharacter.java
@@ -0,0 +1,110 @@
+package Lab_9;
+import java.util.Random;
+
+public class RandomCharacter {
+    // Create a new Random
+    Random random = new Random();
+    // Attributes
+    public char lowerCase;
+    public char upperCase;
+    public char randDigit;
+    public char randChar;
+    public String generatedString;
+
+    // Operations
+    // Get
+    public char getLowerCase() { return this.lowerCase; }
+    public char getUpperCase() { return this.upperCase; }
+    public char getDigit() { return this.randDigit; }
+    public char getChar() { return this.randChar; }
+
+    // Returns a random lowercase letter from
+    // 'a' to 'z'
+    public char getRandomLowerCaseLetter() {
+        // The ASCII value is between 97 - 122 for
+        // lower case characters
+        this.lowerCase = (char) ('a' + random.nextInt(26));
+        return this.lowerCase;
+    }
+
+    // Returns a random uppercase letter from
+    // 'A' to 'Z'
+    public char getRandomUpperCaseLetter() {
+        // The ASCII value is between 60 - 95 for
+        // upper case characters
+        this.upperCase = (char) ('A' + random.nextInt(26));
+        return this.upperCase;
+    }
+
+    // Returns a random digit from
+    // '0' to '9'
+    public char getRandomDigit() {
+        // Returns a number between 0 - 9
+        this.randDigit = (char) ('0' + random.nextInt(10));
+        return this.randDigit;
+    }
+
+    // Assuming that it returns every single
+    // ASCII character
+    public char getRandomCharacter() {
+        // Every valid character in the ASCII table
+        // which starts from '!' (33) to 127
+        this.randChar = (char) ('!' + random.nextInt(95));
+        return this.randChar;
+    }
+
+    // Generates a string of characters
+    // based on the functions above
+    public String generateString(int choice, int num) {
+        // Create a new Stringbuilder
+        StringBuilder sb = new StringBuilder();
+        // Iterate through a switch-case to see
+        // which option the user has selected
+        switch(choice) {
+            // Lowercase string
+            case 1:
+                while (num > 0) {
+                    // Append a lowercase letter into the
+                    // StringBuilder
+                    sb.append(getRandomLowerCaseLetter());
+                    // Decrease 'num' after each iteration
+                    num--;
+                }
+                break;
+            // Uppercase string
+            case 2:
+                while (num > 0) {
+                    // Append an uppercase letter into the
+                    // StringBuilder
+                    sb.append(getRandomUpperCaseLetter());
+                    // Decrease 'num' after each iteration
+                    num--;
+                }
+                break;
+            // Digits
+            case 3:
+                while (num > 0) {
+                    // Append a random digit into the
+                    // StringBuilder
+                    sb.append(getRandomDigit());
+                    // Decrease 'num' after each iteration
+                    num--;
+                }
+            // Random characters
+            case 4:
+                while (num > 0) {
+                    // Append a random character into the
+                    // StringBuilder
+                    sb.append(getRandomCharacter());
+                    // Decrease 'num' after each iteration
+                    num--;
+                }
+            // None of the four options above
+            default:
+                break;
+        }
+        // Convert it from a char to a String
+        this.generatedString = sb.toString();
+        return this.generatedString;
+    }
+}
---
+++ TestRandomCharacter.java
@@ -0,0 +1,145 @@
+package Lab_9;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestRandomCharacter {
+    // Attributes
+    // Static Variables
+    static final int LOWERCHOICE = 1;
+    static final int UPPERCHOICE = 2;
+    static final int DIGITCHOICE = 3;
+    static final int CHARCHOICE = 4;
+    static final int NUMOFSTRING = 15;
+    // Non-static Variables
+    public String lowerString;
+    public String upperString;
+    public String digitString;
+    public String charString;
+    public char compareChar;
+    public int comparePrime;
+    public boolean compareFlag = true;
+
+    // Runs before each test
+    @BeforeEach
+    void setUp() {
+        // Create a new object
+        RandomCharacter rc = new RandomCharacter();
+        // Generate four strings of each type
+        lowerString = rc.generateString(LOWERCHOICE, NUMOFSTRING);
+        upperString = rc.generateString(UPPERCHOICE, NUMOFSTRING);
+        digitString = rc.generateString(DIGITCHOICE, NUMOFSTRING);
+        charString = rc.generateString(CHARCHOICE, NUMOFSTRING);
+    }
+
+    @RepeatedTest(5)
+    @DisplayName("Each method is working as intended")
+    public void testMethods() {
+        // Check that the variables are not null
+        // i.e. containing something
+        assertNotNull(lowerString);
+        assertNotNull(upperString);
+        assertNotNull(digitString);
+        assertNotNull(charString);
+    }
+
+    @RepeatedTest(5)
+    @DisplayName("Generated string is 15 letters")
+    public void testLength() {
+        // Compare the variable with the constant
+        // 'NUMOFSTRING' = 15
+        assertEquals(NUMOFSTRING, lowerString.length());
+        assertEquals(NUMOFSTRING, upperString.length());
+        assertEquals(NUMOFSTRING, digitString.length());
+        assertEquals(NUMOFSTRING, charString.length());
+    }
+
+    @RepeatedTest(5)
+    @DisplayName("Test the lowercase function")
+    public void testLowerCase() {
+        // Iterate through each character in the
+        // 'lowerString' variable
+        for (int i = 0; i < lowerString.length(); i++) {
+            // Store each character
+            compareChar = lowerString.charAt(i);
+            // Check that the character is in lowercase
+            assertTrue(Character.isLowerCase(compareChar));
+        }
+    }
+
+    @RepeatedTest(5)
+    @DisplayName("Test the uppercase function")
+    public void testUpperCase() {
+        // Iterate through each character in the
+        // 'upperString' variable
+        for (int i = 0; i < upperString.length(); i++) {
+            // Store each character
+            compareChar = upperString.charAt(i);
+            // Check that the character is in uppercase
+            assertTrue(Character.isUpperCase(compareChar));
+        }
+    }
+
+    @RepeatedTest(5)
+    @DisplayName("Test the digit function")
+    public void testDigits() {
+        // Iterate through each character in the
+        // 'digitString' variable
+        for (int i = 0; i < digitString.length(); i++) {
+            // Store each character
+            compareChar = digitString.charAt(i);
+            // Check that each character is a digit
+            assertTrue(Character.isDigit(compareChar));
+        }
+    }
+
+    @RepeatedTest(5)
+    @DisplayName("Test the character function")
+    public void testChar() {
+        // Iterate through each character in the
+        // 'charString' variable
+        for (int i = 0; i < charString.length(); i++) {
+            // Store each character
+            compareChar = charString.charAt(i);
+            // Check that each character is a character
+            // from the ASCII table
+            assertTrue((int) compareChar <= 128);
+            assertFalse((int) compareChar > 128 || (int) compareChar < 33);
+        }
+    }
+
+    @RepeatedTest(5)
+    @DisplayName("Test if the generated digit is a prime number")
+    public void testPrime() {
+        // Iterate through each character in the
+        // 'digitString' variable
+        for (int i = 0; i < digitString.length(); i++) {
+            // Store each character
+            compareChar = digitString.charAt(i);
+            // Convert the char into a numerical value
+            comparePrime = Character.getNumericValue(compareChar);
+
+            // A loop to check if the number is divisible
+            // by any number from 2 to number / 2
+            for (int j = 2; j <= comparePrime / 2; j++) {
+                // If the number is divisible by another number,
+                // it is not a prime number
+                if (comparePrime % j == 0) {
+                    // Set the flag to be false
+                    compareFlag = false;
+                    // Break out of the loop
+                    break;
+                }
+            }
+            // Check if it is a prime number, however
+            // this means that the test case only passes
+            // if EVERY digit is prime
+            assertTrue(compareFlag);
+
+            // if (compareFlag) { assertTrue(compareFlag); }
+            // else { assertFalse(compareFlag); }
+        }
+    }
+}
